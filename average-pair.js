b4780e1858328427eb4a43fe4ad9bd856a90c91e
brenden <brendenjarias@gmail.com>
2024-02-15 13:27:43 -0800

    completed average-pair.js

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function calculatedAverage(num1, num2) {
+    return num1 + num2 / 2;
+} 
+
+function averagePair(arr, targetAverage) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right ) {
+        if(calculatedAverage(arr[left], arr[right]) === targetAverage) {
+            return true;
+        } else if (calculatedAverage(arr[left], arr[right]) < targetAverage) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return false;
 }
+
+console.log(averagePair([1, 2, 3], 2.5)); // true
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+console.log(averagePair([], 4)); // false
--- construct-note.js
+++ construct-note.js
@@ -3,17 +3,18 @@ function freqCounter(str) {
     //create frequencey map
     const frequencyMap = new Map();
     //add charcs to map
-    for(let letter of str) {
+    for (let letter of str) {
         frequencyMap.set(letter, frequencyMap.get(letter) + 1 || 1)
     }
     return frequencyMap;
 }
 
 function constructNote(message, letters) {
+    if (letters.length === 0) return false;
     const messageMap = freqCounter(message);
     const lettersMap = freqCounter(letters);
-    for(let letter of messageMap.keys()) {
-        if(messageMap.get(letter) > lettersMap.get(letter)) return false;
+    for (let letter of messageMap.keys()) {
+        if (messageMap.get(letter) > lettersMap.get(letter)) return false;
     }
     return true;
 }
