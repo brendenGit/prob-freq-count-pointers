5290eb68f653aff9d421bd906a846d85f6e75af9
brenden <brendenjarias@gmail.com>
2024-02-15 13:36:06 -0800

    completed two-array-objects.js

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    const obj = {};
+    let valCount = 0;
+    for (let key of keys) {
+        obj[key] = values[valCount] || null;
+        valCount++;
+    }
+    return obj;
 }
+
+console.log(twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3])) // {'a': 1, 'b': 2, 'c': 3, 'd': null}
+console.log(twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4])) // {'a': 1, 'b': 2, 'c': 3}
+console.log(twoArrayObject(['x', 'y', 'z'], [1, 2])) // {'x': 1, 'y': 2, 'z': null}
