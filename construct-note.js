5a3dde65b1b4995c939ddeb0131c7d7eae705eec
brenden <brendenjarias@gmail.com>
2024-02-15 13:18:40 -0800

    created repo completed construct-note.js

---
+++ construct-note.js
@@ -0,0 +1,23 @@
+// add whatever parameters you deem necessary
+function freqCounter(str) {
+    //create frequencey map
+    const frequencyMap = new Map();
+    //add charcs to map
+    for(let letter of str) {
+        frequencyMap.set(letter, frequencyMap.get(letter) + 1 || 1)
+    }
+    return frequencyMap;
+}
+
+function constructNote(message, letters) {
+    const messageMap = freqCounter(message);
+    const lettersMap = freqCounter(letters);
+    for(let letter of messageMap.keys()) {
+        if(messageMap.get(letter) > lettersMap.get(letter)) return false;
+    }
+    return true;
+}
+
+console.log(constructNote('aa', 'abc')) // false
+console.log(constructNote('abc', 'dcba')) // true
+console.log(constructNote('aabbcc', 'bcabcaddff')) // true
